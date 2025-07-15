ef59927518aa1abb9b754acdb2147e2d0c699be5
Chino0402 <ramosrodriguez2003@gmail.com>
2025-07-15 11:19:12 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function desbloquear(id) {
-  const siguiente = document.getElementById(id);
-  if (siguiente) {
-    siguiente.classList.remove('bloqueado');
-    siguiente.scrollIntoView({ behavior: 'smooth' });
-  }
-}
